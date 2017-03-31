f0f1f029617fab1cc8bf50ca746b3595319c98fc
Vladimir Ivanov <Vladimir_Ivanov4@epam.com>
2017-03-31 14:15:14 +0300

    Login to github with basic auth

--- github/src/LoginScreen.js
+++ github/src/LoginScreen.js
@@ -1,43 +1,91 @@
-import React, { Component } from 'react';
-import {Button, Image, TextInput, View} from "react-native";
+import React, {Component} from 'react';
+import {Button, Text, Image, TextInput, View} from "react-native";
+
+import { Buffer } from 'buffer';
 
 export default class LoginScreen extends Component {
 
+    encode(value) {
+        return new Buffer(value).toString('base64');
+    }
+
     onLogin() {
-        console.log('Hello!')
+        this.setState({ error : null });
+        let base64 = this.encode(`${this.state.login}:${this.state.password}`);
+        console.log('fetch: login ' + this.state.login);
+        console.log('fetch: base64 ' + base64);
+        fetch('https://api.github.com/user/repos', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + base64
+            },
+        })
+            .then(result => {
+                console.log('fetch: ' + JSON.stringify(result));
+                if (result.status === 200) {
+                    console.log('fetch: success!')
+                } else {
+                    this.setState({ error: `Failed to login with ${result.status}` })
+                }
+            })
+            .catch(error => {
+                console.log('error: ' + JSON.stringify(error));
+                this.setState({ error: `Failed to login with ${error}` })
+            })
+            .done();
+
     };
 
     constructor(props) {
         super(props);
-        this.state = { login: '', password: '' }
+        this.state = {login: '', password: '', error: null}
     }
 
-    render() {
-        const { container, textInput } = styles;
+    renderInputs() {
+        const {textInput} = styles;
 
         return (
-            <View style={container}>
-                <View style={{ alignItems: 'center' }}>
-                    <Image
-                        source={require('./GitHub-Logo.png')}
-                    />
-                </View>
+            <View>
                 <TextInput
                     style={textInput}
                     placeholder='login'
-                    onTextChanged={(value) => {
-                        this.setState({ login: value });
+                    onChangeText={(value) => {
+                        this.setState({login: value});
                     }}
                 />
                 <TextInput
                     style={textInput}
                     secureTextEntry
                     placeholder='password'
-                    onTextChanged={(value) => {
-                        this.setState({ password: value });
+                    onChangeText={(value) => {
+                        this.setState({password: value});
                     }}
                 />
-                <Button title='Login' onPress={this.onLogin.bind(this)} />
+            </View>);
+    }
+
+    renderError() {
+        return (this.state.error &&
+            <Text style={{fontSize: 14, color: 'red', margin: 16}}>
+                {this.state.error}
+            </Text>);
+    }
+
+    render() {
+        const {container, textInput} = styles;
+
+        return (
+            <View style={container}>
+                <View style={{alignItems: 'center'}}>
+                    <Image
+                        source={require('./GitHub-Logo.png')}
+                    />
+                </View>
+                {this.renderInputs()}
+                {this.renderError()}
+                <Button title='Login' onPress={this.onLogin.bind(this)}/>
             </View>
         )
     }
